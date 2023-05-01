5436b4278e3378cc1faf056018230b1077568d1a
Josh Cline <joshccline@outlook.com>
2023-05-01 13:54:22 -0400

    Add views for index

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const breadController = require('./controllers/bread')
 
 const app = express()
 
-app.use('/bread', breadController)
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// routes
+app.use('/breadS', breadController)
 
 const PORT = process.env.PORT
-
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
