99af9a7ca46fb9afccbe14009ddf82da2280c2bf
Josh Cline <joshccline@outlook.com>
2023-05-14 21:40:27 -0400

    set up database and Get routes

--- controllers/bread.js
+++ controllers/bread.js
@@ -2,22 +2,23 @@ const router = require('express').Router()
 const Bread = require('../models/bread')
 
 // GET all the bread
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const bread = await Bread.find()
   res.render('index', {
-    breads: Bread
+    breads: bread
   })
 })
 
-// GET bread a specific bread by index
 router.get('/new', (req, res) => {
   res.render('new')
 })
 
-router.get('/:index', (req, res) => {
-  const { index } = req.params
+// GET bread a specific bread by index
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const bread = await Bread.findById(id)
   res.render('show', {
-    bread: Bread[index],
-    index: index
+    bread,
   })
 })
 
@@ -30,8 +31,8 @@ router.get('/:index/edit', (req, res) => {
   })
 })
 
-router.post('/', (req, res) => {
-  if (req.body.image) req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+router.post('/', async (req, res) => {
+  if (!req.body.image) req.body.image = undefined
 
   if (req.body.hasGluten === 'on') {
     req.body.hasGluten = true
@@ -39,7 +40,7 @@ router.post('/', (req, res) => {
     req.body.hasGluten = false
   }
 
-  Bread.push(req.body)
+  await Bread.create(req.body)
   res.status(303).redirect('/breads')
 })
 
@@ -51,7 +52,7 @@ router.delete('/:index', (req, res) => {
 
 router.put('/:index', (req, res) => {
   const { index } = req.params
-  if (!req.body.image) req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+  // if (!req.body.image) req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
 
   if (req.body.hasGluten === 'on') {
     req.body.hasGluten = true
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 require('dotenv').config()
 const breadController = require('./controllers/bread')
 
@@ -7,14 +8,19 @@ const app = express()
 
 // MIDDLEWARE
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
 // routes
 app.use('/breads', breadController)
+
+//Database Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('DB Connected'))
+.catch(() => console.error(err))
 
 const PORT = process.env.PORT
 
